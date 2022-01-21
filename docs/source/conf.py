8c6d64d36f1e02d172c05d4dd8cf4f51170a7604
LukasBommes <lukas.bommes@gmx.de>
2022-01-21 17:11:28 +0100

    fix problem with unavailable image in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+html_extra_path = ["images/high_level_overview.png"]
 exclude_patterns = []
 
 
